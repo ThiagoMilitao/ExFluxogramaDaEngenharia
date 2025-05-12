8b95cd9b61d7ba33b17b48b174240a72d002a5c4
thiagoMilitao <thiago.militao2007@gmail.com>
2025-05-12 10:00:37 -0300

    exfluxogramadaengenharia

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("O objeto está se movendo (S/N)?");
+char resposta = char.Parse(Console.ReadLine().ToUpper());
+
+if (resposta == 'S')
+{
+    Console.WriteLine("Deveria? (S/N)");
+    resposta = char.Parse(Console.ReadLine().ToUpper());
+    if (resposta == 'S')
+    {
+        Console.WriteLine("Ótimo!");
+    }
+    else
+    {
+        Console.WriteLine("Use Silver Tape");
+    }
+}
+else
+{
+    Console.WriteLine("Deveria? (S/N)");
+    resposta = char.Parse(Console.ReadLine().ToUpper());
+    if (resposta == 'S')
+    {
+        Console.WriteLine("Use WD-40");
+    }
+    else
+    {
+        Console.WriteLine("Ótimo!");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# ExFluxogramaDaEngenharia
-Faça um programa que implemente o meme Fluxograma da Engenharia.
+Faça um programa que implemente o meme _Fluxograma da Engenharia_.
+
+Exemplos:
+
+```
+--- Fluxograma da Engenharia ---
+
+O objeto está se movendo (S/N)? N
+Deveria? S
+
+Use WD-40.
+```
+
+```
+--- Fluxograma da Engenharia ---
+
+O objeto está se movendo (S/N)? S
+Deveria? N
+
+Use Silver Tape.
+```
+
+```
+--- Fluxograma da Engenharia ---
+
+O objeto está se movendo (S/N)? N
+Deveria? N
+
+Ótimo!
+```
